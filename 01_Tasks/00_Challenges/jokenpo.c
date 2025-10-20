0b837f5afc422b7ebd145e9b7329ed3de6732a51
Jansen Rabelo <rabelo.jansen@icloud.com>
2025-10-20 20:48:20 -0300

    progress

---
+++ 01_Tasks/00_Challenges/jokenpo.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int playerChoice = 0, computerChoice = 0;
+    srand(time(0));
+ 
+        printf("### Jokenpô - Pedra, Papel ou Tesoura ###\n");
+            printf("\n");
+
+        while (1) { // Loop para o menu inicial   
+        printf("MENU INICIAL:\n");
+            printf("\n");
+                printf("(1) Iniciar o jogo\n");
+                printf("(2) Regras do jogo\n");
+                printf("(3) Sair\n");
+            printf("\n");
+                printf("Escolha uma opção: ");
+                    scanf("%d", &playerChoice);
+                printf("\n");
+                    switch (playerChoice) {
+                    case 1:
+                        printf("Iniciando o jogo...\n");
+                            printf("\n");
+                        break;
+                    case 2:
+                        printf("Regras do jogo:\n");
+                            printf("PEDRA vence TESOURA\n");
+                            printf("TESOURA vence PAPEL\n");
+                            printf("PAPEL vence PPEDRA\n");
+                            printf("Em caso de empate, ninguém ganha!\n");
+                            printf("\n");
+                            printf("Pressione ENTER para voltar ao MENU INICIAL.\n");
+                            getchar();
+                            getchar();
+                        continue; // Volta ao menu inicial
+                    case 3:
+                        printf("Saindo do jogo. Até a próxima!\n");
+                            return 0;
+                    default:
+                        printf("Opção inválida, tente novamente...\n");
+                            continue; // Volta ao menu inicial
+                    }
+
+                printf("Escolha sua jogada:\n");
+                    printf("(1) PEDRA\n");
+                    printf("(2) PAPEL\n");
+                    printf("(3) TESOURA\n");
+                printf("\n");
+                    printf("Opção: ");
+                        scanf("%d", &playerChoice);
+                    printf("\n");
+        
+                computerChoice = (rand() % 3) + 1; // Gera número aleatório entre 1 e 3 para o computador
+
+                switch (playerChoice) {
+                case 1:
+                    printf("Você escolheu PEDRA.\n");
+                    break;
+                case 2:
+                    printf("Você escolheu PAPEL.\n");
+                    break;
+                case 3:
+                    printf("Você escolheu TESOURA.\n");
+                    break;
+                default:
+                    printf("Opção inválida! Saindo do jogo.\n");
+                        return 0;
+                    break;
+                }
+
+                switch (computerChoice) {
+                case 1:
+                    printf("O computador escolheu PEDRA.\n");
+                    break;
+                case 2:
+                    printf("O computador escolheu PAPEL.\n");
+                    break;
+                case 3:
+                    printf("O computador escolheu TESOURA.\n");
+                    break;
+                }
+
+                if (computerChoice == playerChoice) {
+                    printf("\n### EMPATE! ###\n");
+                } else if (playerChoice == 1 && computerChoice == 3 || // Pedra vence Tesoura
+                        playerChoice == 2 && computerChoice == 1 || // Papel vence Pedra
+                        playerChoice == 3 && computerChoice == 2) { // Tesoura vence Papel
+                    printf("\n### VOCÊ VENCEU! ###\n");
+                } else {
+                    printf("\n### O COMPUTADOR VENCEU! ###\n");
+                }
+                
+                printf("\n");
+            printf("Fim do Jokenpô.\n");
+        return 0;
+    }
+    return 0;
+}
